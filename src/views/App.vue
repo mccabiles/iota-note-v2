0a490c508ba9047e0db3b9bb39af80da818d01b1
Itio Duo <jiggz.cabiles@gmail.com>
2019-04-11 19:18:48 +0800

    Create Index and App views

---
+++ src/views/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <app-toolbar/>
+    <app-navigation/>
+    <v-content>
+      <router-view/>
+      <create-dialog v-model="showCreate"/>
+      <v-btn color="accent" fab fixed bottom right large @click="showCreate = !showCreate">
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import AppToolbar from '@/components/AppToolbar'
+import AppNavigation from '@/components/AppNavigation'
+import CreateDialog from './Create'
+  export default {
+    name: 'IndexView',
+    components: {
+      AppNavigation,
+      AppToolbar,
+      CreateDialog,
+    },
+    
+    data() {
+      return {
+        showCreate: false,
+      };
+    },
+  }
+</script>
---
+++ src/views/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-layout column>
+    <v-tabs grow v-model="activeTab">
+      <v-tab> All Notes </v-tab>
+      <v-tab> For Review </v-tab>
+    </v-tabs>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
