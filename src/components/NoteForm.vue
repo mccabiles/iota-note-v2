34cec2bb2b6609dfad1e089e90cd605fcdd33e41
Itio Duo <jiggz.cabiles@gmail.com>
2019-04-11 19:18:02 +0800

    Create note details form

---
+++ src/components/NoteForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-form>
+    <v-text-field label="Title" :hint="titleHint" v-model="noteData.title"/>
+    <v-textarea label="Notes" :hint="bodyHint" v-model="noteData.body"/>
+  </v-form>
+</template>
+
+<script>
+class Note {
+  constructor() {
+    this.title = "New Note";
+    this.body = "";
+  }
+}
+
+export default {
+  name: 'NoteForm',
+  props: {
+    note: {
+      type: Object,
+      default: () => new Note(),
+    }
+  },
+
+  computed: {
+    titleHint() { return 'Give your note a title'; },
+    bodyHint() { return 'No need to be too neat at this point!'; },
+  },
+
+  data() {
+    return {
+      noteData: new Note(),
+    };
+  },
+
+  watch: {
+    'note': {
+      immediate: true,
+      handler() {
+        this.noteData = this.note;
+      }
+    }
+  },
+}
+</script>
