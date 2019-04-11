44f535aa98e56a4fc129320659d2f862e3597a96
Itio Duo <jiggz.cabiles@gmail.com>
2019-04-11 19:19:11 +0800

    Fix styling and icon of toolbar

--- src/components/AppToolbar.vue
+++ src/components/AppToolbar.vue
@@ -1,9 +1,9 @@
 <template>
-  <v-toolbar app>
-    <v-btn icon><v-icon>options_vertical</v-icon></v-btn>
+  <v-toolbar app color="primary" dark>
+    <v-btn icon><v-icon>mdi-menu</v-icon></v-btn>
     <v-toolbar-title class="headline text-uppercase">
       <span>Iota Note</span>
-      <span class="font-weight-light">v2</span>
+      <span class="font-weight-light text-sm">v2</span>
     </v-toolbar-title>
     <v-spacer></v-spacer>
     <v-btn
