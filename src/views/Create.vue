257754c36ca5a8a827174d2b9725f78be82a550e
Itio Duo <jiggz.cabiles@gmail.com>
2019-04-11 19:18:59 +0800

    Add Note creation dialog

---
+++ src/views/Create.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-dialog fullscreen :value="value" v-on="$listeners">
+    <v-card>
+      <v-toolbar dark class="title" color="primary" flat>
+        Create New Note
+        <v-spacer/>
+        <v-btn flat icon @click="$emit('input', false)"><v-icon>mdi-close</v-icon></v-btn>
+      </v-toolbar>
+
+      <v-card-text>
+        <note-form />
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import NoteForm from '@/components/NoteForm'
+
+export default {
+  name: 'NoteCreateView',
+  components: {
+    NoteForm,
+  },
+
+  props: {
+    value: Boolean
+  }
+}
+</script>
