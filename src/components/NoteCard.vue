63835a26b96a978f94366549fc2a67acc3a40a9e
Itio Duo <jiggz.cabiles@gmail.com>
2019-05-11 22:27:48 +0800

    Create Note Card component

---
+++ src/components/NoteCard.vue
@@ -0,0 +1,25 @@
+<template>
+	<v-flex md4 p-4>
+		<v-card @click="viewNote">
+			<v-card-title class="title">
+				{{ note.title }}
+			</v-card-title>
+
+			<v-card-text>
+				{{ note.body }}
+			</v-card-text>
+		</v-card>
+	</v-flex>
+</template>
+
+<script>
+export default {
+	name: 'NoteCard',
+	props: [ 'note' ],
+	methods: {
+		viewNote() {
+			this.$router.push({ name: 'view.note', params: { id: this.note.id } });
+		},
+	},
+}
+</script>
